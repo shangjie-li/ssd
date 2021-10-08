f19b6f9978ecf0349c51d4a2724a37a16658b4d7
shangjie-li <1814481684@qq.com>
2021-10-08 23:13:06 +0800

    Add Expand & RandomSampleCrop code

--- data/augmentations.py
+++ data/augmentations.py
@@ -3,6 +3,19 @@ import numpy as np
 import cv2
 from numpy import random
 
+def intersect(boxes, box):
+    max_xy = np.minimum(boxes[:, 2:], box[2:])
+    min_xy = np.maximum(boxes[:, :2], box[:2])
+    dxdy = np.clip((max_xy - min_xy), a_min=0, a_max=np.inf)
+    return dxdy[:, 0] * dxdy[:, 1]
+
+def jaccard_numpy(boxes, box):
+    inter = intersect(boxes, box)
+    area_a = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
+    area_b = (box[2] - box[0]) * (box[3] - box[1])
+    union = area_a + area_b - inter
+    return inter / union
+
 class Compose():
     def __init__(self, transforms):
         self.transforms = transforms
@@ -14,15 +27,6 @@ class Compose():
 
 class ToFloats():
     def __call__(self, img, boxes=None, labels=None):
-        return img.astype(np.float32), boxes, labels
-
-class SubtractMeans():
-    def __init__(self, mean):
-        self.mean = np.array(mean, dtype=np.float32)
-        
-    def __call__(self, img, boxes=None, labels=None):
-        img = img.astype(np.float32)
-        img -= self.mean
         return img.astype(np.float32), boxes, labels
 
 class ToAbsoluteCoords():
@@ -43,12 +47,31 @@ class ToPercentCoords():
         boxes[:, 3] /= h
         return img, boxes, labels
 
-class Resize():
-    def __init__(self, size):
-        self.size = size
+class RandomContrast():
+    def __init__(self, lower=0.5, upper=1.5):
+        self.lower = lower
+        self.upper = upper
+        assert self.upper >= self.lower
+        assert self.lower >= 0
         
     def __call__(self, img, boxes=None, labels=None):
-        img = cv2.resize(img, (self.size, self.size)) # assume coords are percent form 
+        if random.randint(2): # randomly create a number between [0, 2)
+            alpha = random.uniform(self.lower, self.upper)
+            img *= alpha
+        return img, boxes, labels
+
+class ConvertColor():
+    def __init__(self, current='BGR', transform='HSV'):
+        self.current = current
+        self.transform = transform
+        
+    def __call__(self, img, boxes=None, labels=None):
+        if self.current == 'BGR' and self.transform == 'HSV':
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        elif self.current == 'HSV' and self.transform == 'BGR':
+            img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+        else:
+            raise NotImplementedError
         return img, boxes, labels
 
 class RandomSaturation():
@@ -75,6 +98,18 @@ class RandomHue():
             img[:, :, 0][img[:, :, 0] < 0.0] += 360.0
         return img, boxes, labels
 
+class RandomBrightness():
+    def __init__(self, delta=32):
+        assert delta >= 0.0
+        assert delta <= 255.0
+        self.delta = delta
+        
+    def __call__(self, img, boxes=None, labels=None):
+        if random.randint(2): # randomly create a number between [0, 2)
+            delta = random.uniform(-self.delta, self.delta)
+            img += delta
+        return img, boxes, labels
+
 class RandomLightingNoise():
     def __init__(self):
         self.perms = (
@@ -87,45 +122,6 @@ class RandomLightingNoise():
         if random.randint(2): # randomly create a number between [0, 2)
             swap = self.perms[random.randint(len(self.perms))]
             img = img[:, :, swap]
-        return img, boxes, labels
-
-class ConvertColor():
-    def __init__(self, current='BGR', transform='HSV'):
-        self.current = current
-        self.transform = transform
-        
-    def __call__(self, img, boxes=None, labels=None):
-        if self.current == 'BGR' and self.transform == 'HSV':
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        elif self.current == 'HSV' and self.transform == 'BGR':
-            img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-        else:
-            raise NotImplementedError
-        return img, boxes, labels
-
-class RandomContrast():
-    def __init__(self, lower=0.5, upper=1.5):
-        self.lower = lower
-        self.upper = upper
-        assert self.upper >= self.lower
-        assert self.lower >= 0
-        
-    def __call__(self, img, boxes=None, labels=None):
-        if random.randint(2): # randomly create a number between [0, 2)
-            alpha = random.uniform(self.lower, self.upper)
-            img *= alpha
-        return img, boxes, labels
-
-class RandomBrightness():
-    def __init__(self, delta=32):
-        assert delta >= 0.0
-        assert delta <= 255.0
-        self.delta = delta
-        
-    def __call__(self, img, boxes=None, labels=None):
-        if random.randint(2): # randomly create a number between [0, 2)
-            delta = random.uniform(-self.delta, self.delta)
-            img += delta
         return img, boxes, labels
 
 class PhotometricDistort():
@@ -151,27 +147,31 @@ class PhotometricDistort():
         img, boxes, labels = t(img, boxes, labels)
         return self.rln(img, boxes, labels)
 
-class RandomMirror():
+class Expand():
+    def __init__(self, mean):
+        self.mean = mean
+        
     def __call__(self, img, boxes, labels):
-        h, w, _ = img.shape
-        if random.randint(2): # randomly create a number between [0, 2)
-            img = img[:, ::-1]
-            boxes = boxes.copy()
-            boxes[:, 0::2] = w - boxes[:, 2::-2] # (x1, x2) = w - (x2, x1)
-        return img, boxes, labels
+        if random.randint(2):
+            return img, boxes, labels
+        
+        height, width, depth = img.shape
+        img, boxes, labels = img.copy(), boxes.copy(), labels.copy()
+        
+        ratio = random.uniform(1, 4)
+        left = random.uniform(0, width * ratio - width)
+        top = random.uniform(0, height * ratio - height)
+        
+        h, w = int(height * ratio), int(width * ratio)
+        expand_img = np.zeros((h, w, depth), dtype=img.dtype)
+        expand_img[:, :, :] = self.mean
+        expand_img[int(top):int(top + height), int(left):int(left + width)] = img
+        
+        expand_boxes = boxes
+        expand_boxes[:, :2] += (int(left), int(top))
+        expand_boxes[:, 2:] += (int(left), int(top))
 
-def intersect(boxes, box):
-    max_xy = np.minimum(boxes[:, 2:], box[2:])
-    min_xy = np.maximum(boxes[:, :2], box[:2])
-    dxdy = np.clip((max_xy - min_xy), a_min=0, a_max=np.inf)
-    return dxdy[:, 0] * dxdy[:, 1]
-
-def jaccard_numpy(boxes, box):
-    inter = intersect(boxes, box)
-    area_a = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
-    area_b = (box[2] - box[0]) * (box[3] - box[1])
-    union = area_a + area_b - inter
-    return inter / union
+        return expand_img, expand_boxes, labels
 
 class RandomSampleCrop():
     def __init__(self):
@@ -238,31 +238,31 @@ class RandomSampleCrop():
 
                 return current_img, current_boxes, current_labels
 
-class Expand():
-    def __init__(self, mean):
-        self.mean = mean
-        
+class RandomMirror():
     def __call__(self, img, boxes, labels):
-        if random.randint(2):
-            return img, boxes, labels
-        
-        height, width, depth = img.shape
-        img, boxes, labels = img.copy(), boxes.copy(), labels.copy()
-        
-        ratio = random.uniform(1, 4)
-        left = random.uniform(0, width * ratio - width)
-        top = random.uniform(0, height * ratio - height)
-        
-        h, w = int(height * ratio), int(width * ratio)
-        expand_img = np.zeros((h, w, depth), dtype=img.dtype)
-        expand_img[:, :, :] = self.mean
-        expand_img[int(top):int(top + height), int(left):int(left + width)] = img
-        
-        expand_boxes = boxes
-        expand_boxes[:, :2] += (int(left), int(top))
-        expand_boxes[:, 2:] += (int(left), int(top))
+        h, w, _ = img.shape
+        if random.randint(2): # randomly create a number between [0, 2)
+            img = img[:, ::-1]
+            boxes = boxes.copy()
+            boxes[:, 0::2] = w - boxes[:, 2::-2] # (x1, x2) = w - (x2, x1)
+        return img, boxes, labels
 
-        return expand_img, expand_boxes, labels
+class Resize():
+    def __init__(self, size):
+        self.size = size
+        
+    def __call__(self, img, boxes=None, labels=None):
+        img = cv2.resize(img, (self.size, self.size)) # assume coords are percent form 
+        return img, boxes, labels
+
+class SubtractMeans():
+    def __init__(self, mean):
+        self.mean = np.array(mean, dtype=np.float32)
+        
+    def __call__(self, img, boxes=None, labels=None):
+        img = img.astype(np.float32)
+        img -= self.mean
+        return img.astype(np.float32), boxes, labels
 
 class Augmentation():
     def __init__(self, size, mean):
